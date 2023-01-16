8242ea2ee254ef63d033650778e4867c98e24904
80847835 <leonardo.s@compasso.com.br>
2023-01-16 12:45:06 -0300

    database instantiation

--- src/main/java/com/leonardooliveira/courseproject/repository/UserRepository.java
+++ src/main/java/com/leonardooliveira/courseproject/repository/UserRepository.java
@@ -3,11 +3,9 @@ package com.leonardooliveira.courseproject.repository;
 import java.io.Serializable;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import com.leonardooliveira.courseproject.domain.User;
 
-@Repository
 public interface UserRepository extends MongoRepository<User, Serializable> {
 
 }
