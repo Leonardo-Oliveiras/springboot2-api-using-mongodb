b0d9c8cd222cbc103ab8af6c827f0fbca0c05142
80847835 <leonardo.s@compasso.com.br>
2023-01-16 16:10:45 -0300

    Simple querying (by MongoDB)

--- src/main/java/com/leonardooliveira/courseproject/repository/PostRepository.java
+++ src/main/java/com/leonardooliveira/courseproject/repository/PostRepository.java
@@ -1,12 +1,17 @@
 package com.leonardooliveira.courseproject.repository;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.leonardooliveira.courseproject.domain.Post;
-import com.leonardooliveira.courseproject.domain.User;
 
 public interface PostRepository extends MongoRepository<Post, Serializable> {
-
+	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
